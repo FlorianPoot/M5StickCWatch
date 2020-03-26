b71c7742ae54c25f6e4d0122a9d6ad6565282ffb
Florian Poot <florian.poot@gmail.com>
2020-03-26 11:41:06 +0100

    Update battery

--- data/battery.py
+++ data/battery.py
@@ -2,6 +2,7 @@ from hardware import *
 from machine import Timer
 
 import data.menu
+import time
 
 
 class Battery:
@@ -10,51 +11,75 @@ class Battery:
 
         """Initialize Battery."""
 
+        self.bar_pos = 0
+
         lcd.clear()
         lcd.font("data/fonts/aril24.fon")
         lcd.set_bg(lcd.BLACK)
 
         buttonA.was_pressed(callback=self.exit)
-        buttonB.was_pressed(callback=lambda: None)
+        buttonB.clear()
 
-        self.show_battery()
+        # Battery icon.
+        lcd.rect(12, 22, 10, 16, lcd.BLACK, lcd.BLACK)
+        # Percentage rectangle.
+        lcd.roundrect(45, 58, 70, 20, 8, lcd.BLACK, lcd.BLACK)
 
         self.refresh = Timer(0)
         self.refresh.init(period=10000, mode=Timer.PERIODIC, callback=self.show_battery)
+
+        self.show_battery()
 
     def show_battery(self, t=None):
 
         """Convert battery voltage into bars."""
 
-        bat_data = axp.get_vbat_data() * 1.1
-
-        vbat = self.map_value(bat_data, 3000, 4100, 0, 7)
-        pbat = min(self.map_value(bat_data, 3000, 4100, 0, 100), 100)
-
-        print(bat_data)
-        print(vbat)
-        print(pbat)
-
         if axp.get_icharge_data() / 2 > 0:
-            color = lcd.YELLOW
-        elif vbat == 1:
-            color = lcd.RED
-        elif vbat == 2:
-            color = lcd.ORANGE
+            # If pluged in.
+            self.refresh.period(500)
+
+            if self.bar_pos == 0:
+                # Battery Icon.
+                lcd.rect(22, 5, 125, 50, lcd.BLACK, lcd.BLACK)
+
+                lcd.roundrect(45, 58, 70, 20, 8, lcd.BLACK, lcd.BLACK)
+                lcd.text(lcd.CENTER, 63, "--%", lcd.WHITE)
+
+            else:
+                lcd.rect(((6 - self.bar_pos) * 20) + 27, 10, 15, 40, lcd.GREEN, lcd.GREEN)
+
+            self.bar_pos += 1
+            self.bar_pos %= 7
+
         else:
-            color = lcd.GREEN
+            self.refresh.period(10000)
 
-        # Battery Icon.
-        lcd.rect(22, 5, 125, 50, lcd.BLACK, lcd.BLACK)
-        lcd.rect(12, 22, 10, 16, lcd.BLACK, lcd.BLACK)
+            bat_data = axp.get_vbat_data() * 1.1
 
-        # Draw bars.
-        for i in range(1, 7):
-            if vbat >= i:
-                lcd.rect(((6 - i) * 20) + 27, 10, 15, 40, color, color)
+            vbat = round(self.map_value(bat_data, 3000, 4100, 0, 7))
+            pbat = max(min(round(self.map_value(bat_data, 3000, 4100, 0, 100)), 100), 0)
 
-        lcd.roundrect(45, 58, 70, 20, 8, lcd.BLACK, lcd.BLACK)
-        lcd.text(lcd.CENTER, 63, str(round(pbat)) + "%", lcd.WHITE)
+            print(bat_data)
+            print(vbat)
+            print(pbat)
+
+            # Battery Icon.
+            lcd.rect(22, 5, 125, 50, lcd.BLACK, lcd.BLACK)
+
+            if vbat == 1:
+                color = lcd.RED
+            elif vbat == 2:
+                color = lcd.ORANGE
+            else:
+                color = lcd.GREEN
+
+            # Draw bars.
+            for i in range(1, 7):
+                if vbat >= i:
+                    lcd.rect(((6 - i) * 20) + 27, 10, 15, 40, color, color)
+
+            lcd.roundrect(45, 58, 70, 20, 8, lcd.BLACK, lcd.BLACK)
+            lcd.text(lcd.CENTER, 63, str(pbat) + "%", lcd.WHITE)
 
     def exit(self):
 
